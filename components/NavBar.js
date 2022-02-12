7a0f3a18a2300eafe0b717df3e59149f81c28dfc
트립비토즈 김수영 <ksy9926@naver.com>
2022-02-12 23:11:14 +0900

    Feat: Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 
 const NavBar = () => {
   const router = useRouter();
@@ -9,11 +8,22 @@ const NavBar = () => {
     <nav>
       {/* Link 에는 className, style등이 적용되지 않는다. 단지 웹사이트 리로딩을 막는 용도로 href만 사용을 하며 나머지 모든 속성은 a tag를 통해서 적용한다. */}
       <Link href='/'>
-        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href='/about'>
-        <a className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}>About</a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 };
