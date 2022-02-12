d66a546b5c5497048d1a93acef6cb717fc5b21bf
트립비토즈 김수영 <ksy9926@naver.com>
2022-02-12 23:04:22 +0900

    Feat: CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './NavBar.module.css';
 
 const NavBar = () => {
   const router = useRouter();
@@ -7,11 +8,11 @@ const NavBar = () => {
   return (
     <nav>
       {/* Link 에는 className, style등이 적용되지 않는다. 단지 웹사이트 리로딩을 막는 용도로 href만 사용을 하며 나머지 모든 속성은 a tag를 통해서 적용한다. */}
-      <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+      <Link href='/'>
+        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
       </Link>
-      <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a>
+      <Link href='/about'>
+        <a className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}>About</a>
       </Link>
     </nav>
   );
